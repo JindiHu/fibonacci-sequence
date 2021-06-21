bb1b8c7e67bb75ebd5eafa7aa6f5dedafba86e4c
Hu Jindi <hujindi1212@gmail.com>
2021-06-21 22:23:12 +0800

    added worker

---
+++ worker/index.ts
@@ -0,0 +1,21 @@
+import keys from "./keys";
+import redis from "redis";
+
+const publisher: any = redis.createClient({
+	host: keys.redisHost,
+	port: keys.redisPort,
+	retry_strategy: () => 1000,
+});
+
+const subscriber: any = publisher.duplicate();
+
+const fib = (index: number): number => {
+	if (index < 2) return 1;
+	return fib(index - 1) + fib(index - 2);
+};
+
+subscriber.on("message", (channel, message) => {
+	publisher.hmset("values", message, fib(parseInt(message)));
+});
+
+subscriber.subscribe("insert");
---
+++ worker/keys.ts
@@ -0,0 +1,5 @@
+const keys = {
+	redisHost: process.env.REDIS_HOST,
+	redisPort: process.env.REDIS_PORT,
+};
+export default keys;
